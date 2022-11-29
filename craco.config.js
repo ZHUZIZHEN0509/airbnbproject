72fce5b470b70e75ef31f2f8e8ce274daa5fe3cd
ZHUZIZHEN0509 <2337658094@qq.com>
2022-11-29 20:33:03 +0800

    集成material-ui库

--- craco.config.js
+++ craco.config.js
@@ -1,18 +1,19 @@
 const CracoLessPlugin = require("craco-less");
 const path = require("path");
-const resolve = (filepath) => path.resolve(__dirname,filepath);
+const resolve = (filepath) => path.resolve(__dirname, filepath);
 
 module.exports = {
-    plugins: [
-        {
-            plugin: CracoLessPlugin
-        }
-    ],
-    webpack: {
-        alias: {
-            "@": resolve("src"),
-            "components": resolve("src/components"),
-            "utils": resolve("src/utils")
-          }
-    }
-}
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+    },
+  ],
+  webpack: {
+    alias: {
+      "@": resolve("src"),
+      components: resolve("src/components"),
+      utils: resolve("src/utils"),
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    },
+  },
+};

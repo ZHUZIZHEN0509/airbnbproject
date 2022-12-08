03f75dec98c8ca302a72bdabe39d093bfa96025c
ZHUZIZHEN0509 <2337658094@qq.com>
2022-12-08 10:37:55 +0800

    main多个页面切换滚动到顶部

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useRoutes } from "react-router-dom";
 import routes from "./router";
 import AppHeader from "./components/app-header";
 import AppFooter from "./components/app-footer";
+import useScrollTop from "./hooks/useScrollTop";
 
 function App() {
+  //解决在上一个页面滚动到底部，下一个页面没有恢复
+  useScrollTop();
   return (
     <div className="App">
       <AppHeader />
